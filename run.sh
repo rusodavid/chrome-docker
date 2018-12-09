09e32e89ccfa733f5bff12e0f66e8526c562d706
David <rusodavid@gmail.com>
2018-12-10 00:45:21 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+# Run Chrome in a container
+
+docker run --rm -it --net host --cpuset-cpus 0 --memory 512mb -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v /data/chrome/Downloads:/home/chrome/Downloads -v /data/chrome/data:/data --device /dev/snd --device /dev/dri -v /dev/shm:/dev/shm --security-opt seccomp=/data/chrome/chrome.json --name chrome chrome --no-sandbox
+
+# You will want the custom seccomp profile:
+#       wget https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/docker/seccomp/chrome.json -O ~/chrome.json
+#-v /data/chrome/.config/google-chrome/:/home/chrome/.config/google-chrome
