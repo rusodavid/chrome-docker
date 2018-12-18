963be18460016317aef1f9c2a4286de93b9e2c98
David <rusodavid@gmail.com>
2018-12-18 01:09:04 +0100

    X11 set up

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # Run Chrome in a container
-
+xhost local:root
 docker run --rm -it --net host --cpuset-cpus 0 --memory 512mb -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v /data/chrome/Downloads:/home/chrome/Downloads -v /data/chrome/data:/data --device /dev/snd --device /dev/dri -v /dev/shm:/dev/shm --security-opt seccomp=/data/chrome/chrome.json --name chrome chrome --no-sandbox
 
 # You will want the custom seccomp profile:
